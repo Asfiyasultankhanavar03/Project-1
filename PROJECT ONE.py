6fb3841772a3aec23042f5df0ccbb89f2c3e1634
Asfiya Sultankhanavar <66782378+Asfiyasultankhanavar03@users.noreply.github.com>
2020-07-16 12:56:44 +0530

    Add files via upload

---
+++ PROJECT ONE.py
@@ -0,0 +1,163 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  3 19:00:49 2020
+
+@author: USER
+"""
+
+
+#IMPORTING THE LIBRARES
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+#READING TH DATA FEOM YOUR FILES
+data = pd.read_csv("advertising.csv")
+data.head()
+
+
+
+#TO VISUALISE DATA
+fig , axs = plt.subplots(1,3,sharey = True)
+data.plot(kind='scatter',x='TV',y='Sales',ax=axs[0],figsize=(16,8))
+data.plot(kind='scatter',x='Radio',y='Sales',ax=axs[1])
+data.plot(kind='scatter',x='Newspaper',y='Sales',ax=axs[2])
+
+
+
+#CREATING X&Y FOR LINEAR REGRESSION 
+feature_cols = ['TV']
+X = data[feature_cols]
+y = data.Sales
+
+
+#IMPORTING LINEAR REGRESSION ALGORITHM FOR SIMPLE LINEAR REGRESSION
+from sklearn.linear_model import LinearRegression
+lr = LinearRegression()
+lr.fit(X,y)
+
+
+print(lr.intercept_)
+print(lr.coef_)
+
+
+
+
+
+
+result = 6.97+0.055*50
+print(result)
+
+
+
+#CREATE A DATAFRAME WITH MIN AND MAX VALUES OF THE TABLE
+X_new = pd.DataFrame({'TV':[data.TV.min(),data.TV.max()]})
+X_new.head()
+
+
+
+preds = lr.predict(X_new)
+preds
+
+
+data.plot(kind='scatter',x='TV',y='Sales')
+
+plt.plot(X_new,preds,c='red',linewidth=3)
+
+import statsmodels.formula.api as smf
+lm = smf.ols(formula= 'Sales ~ TV',data=data).fit()
+lm.conf_int()
+
+
+#FINDING THE PROBABILITY VALUES
+lm.pvalues
+
+
+
+#FINDING THE R-SQUARED VALUES
+lm.rsquared
+
+
+
+#MULTI LINEAR REGRESSION
+feature_cols = ['TV','Radio','Newspaper']
+X = data[feature_cols]
+y = data.Sales
+
+
+lr = LinearRegression()
+lr.fit(X,y)
+
+print(lr.intercept_)
+print(lr.coef_)
+
+
+lm = smf.ols(formula='Sales ~ TV+Radio+Newspaper',data=data).fit()
+lm.conf_int()
+lm.summary()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
